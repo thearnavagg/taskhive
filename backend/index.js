1ac1e5d69c1659782052bed1d7ecd574740ca7a1
Arnav Aggarwal <arnavaggarwal2012@gmail.com>
2024-07-07 22:59:25 +0530

    solving vercel no route error

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,6 @@ dotenv.config();
 
 dbConnection();
 
-const PORT = process.env.PORT || 5000;
-
 const app = express();
 
 app.use(
@@ -34,41 +32,4 @@ app.use("/api", routes);
 app.use(routeNotFound);
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
-
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import express from "express";
-// import morgan from "morgan";
-// import { errorHandler, routeNotFound } from "./middlewares/errorMiddlewaves.js";
-// import routes from "./routes/index.js";
-// import { dbConnection } from "./utils/index.js";
-
-// dotenv.config();
-
-// dbConnection();
-
-// const app = express();
-
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000", "thetaskhive.vercel.app"],
-//     methods: ["GET", "POST", "DELETE", "PUT"],
-//     credentials: true,
-//   })
-// );
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use(cookieParser());
-
-// app.use(morgan("dev"));
-// app.use("/api", routes);
-
-// app.use(routeNotFound);
-// app.use(errorHandler);
-
-// // Export the app for Vercel to handle
-// export default app;
+export default app;
